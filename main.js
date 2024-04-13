9c3330749b41043638c8bee62c1e078150533cf1
Ebubechukwu Ugbor <73425258+3bube@users.noreply.github.com>
2024-04-13 10:39:46 -0700

    Update main.js
    fixed the displayAdvice function to display quotes as many times onclick

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
 const adviceApi = 'https://api.adviceslip.com/advice';
 let advice;
+let id;
 
-function getAdvice(){
-    return fetch(adviceApi)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    advice = data.slip.advice;
-    return advice;
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-}
-
-getAdvice().then(advice => {
-    displayAdvice(advice);
-});
-
-  function displayAdvice(advice){
+  
+  function displayAdvice() {
     const dice = document.querySelector('.dice');
-    dice.removeEventListener('click', clickHandler);
-    clickHandler();
-    function clickHandler (){
-        dice.addEventListener('click', ()=>{
+    const heading = document.querySelector('.card-heading');
+  
+    async function clickHandler() {
+        try {
+            const response = await fetch(adviceApi);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            console.log(data);
+            const advice = data.slip.advice;
+            const id = data.slip.id;
+  
             const adviceText = document.querySelector('.advice');
-        adviceText.innerHTML = advice;
-        });
+            adviceText.innerHTML = advice;
+            heading.innerHTML = `advice #${id}`;
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
-    
+  
+    dice.removeEventListener('click', clickHandler);
+    dice.addEventListener('click', clickHandler);
   }
   
+  displayAdvice();
